8c9507eb9662ed420fb8ebf81c3988100dec981e
Dr-Noob <peibolms@gmail.com>
2021-02-18 18:57:57 +0100

    [v1.13] Fix CMake build when CMake can not find CUDA

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ rm -rf build/ peakperf
 mkdir build/
 cd build/
 
+# In case you have CUDA installed but it is not detected,
+# set CMAKE_CUDA_COMPILER to your nvcc binary:
+# cmake -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc ..
 cmake ..
 make
 cd -
