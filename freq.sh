22635b66941b22e0974707a1060603a0e5846b32
Dr-Noob <peibolms@gmail.com>
2021-02-20 22:52:42 +0100

    [v1.13] Add freq script mode for GPU. Fix arch CUDA flags in CMake

--- freq.sh
+++ freq.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-watch -n0.2 "grep 'MHz' /proc/cpuinfo | sort -nr && sensors"
+if [ "$1" == "gpu" ]
+then
+  watch -n0.5 "nvidia-smi -i 0 --query-gpu=clocks.current.sm,pstate,fan.speed,temperature.gpu --format=csv"
+else
+  watch -n0.2 "grep 'MHz' /proc/cpuinfo | sort -nr && sensors"
+fi
