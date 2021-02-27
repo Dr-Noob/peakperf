faa63640a8177ce64b13a33afdad70fcd38c9baf
Dr-Noob <peibolms@gmail.com>
2021-02-27 13:43:46 +0100

    [v1.13] Small changes to freq.sh script

--- freq.sh
+++ freq.sh
@@ -2,7 +2,9 @@
 
 if [ "$1" == "gpu" ]
 then
-  watch -n0.5 "nvidia-smi -i 0 --query-gpu=clocks.current.sm,pstate,fan.speed,temperature.gpu --format=csv"
+  # nvidia-smi --help-query-gpu
+  # sudo nvidia-smi -i 0 -pm 1
+  watch -n0.2 "nvidia-smi -i 0 --query-gpu=utilization.gpu,clocks.current.sm,pstate,fan.speed,temperature.gpu --format=csv"
 else
   watch -n0.2 "grep 'MHz' /proc/cpuinfo | sort -nr && sensors"
 fi
