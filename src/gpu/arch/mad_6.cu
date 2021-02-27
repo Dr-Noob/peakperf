e569511bd8018a4ca988ef2730669775c4612f74
Dr-Noob <peibolms@gmail.com>
2021-02-27 15:43:06 +0100

    [v1.13e] Add fixed number of unroll. All threads write and read from memory to the same place

--- src/gpu/arch/mad_6.cu
+++ src/gpu/arch/mad_6.cu
@@ -2,14 +2,14 @@
 
 __global__
 void compute_mad_6(float *vec_a, float *vec_b, float *vec_c, int n) {
-  int idx = (blockIdx.x * blockDim.x + threadIdx.x);
-  float a = vec_a[idx];
-  float b = vec_b[idx];
+  float a = vec_a[0];
+  float b = vec_b[0];
   float c = 0.0;
 
+  #pragma unroll 3000
   for(long i=0; i < BENCHMARK_GPU_ITERS; i++) {
     c = (c * a) + b;
   }
 
-  vec_c[idx] = c;
+  vec_c[0] = c;
 }
