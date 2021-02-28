ceb29f04b7661a326d85ba9733aa5052ff05ce7e
Dr-Noob <peibolms@gmail.com>
2021-02-28 10:19:00 +0100

    [v1.14] Fix build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # peakperf build script
 set -e
 
-#rm -rf build/ peakperf
-#mkdir build/
+rm -rf build/ peakperf
+mkdir build/
 cd build/
 
 # In case you have CUDA installed but it is not detected,
@@ -12,6 +12,6 @@ cd build/
 # - set CMAKE_CUDA_COMPILER_TOOLKIT_ROOT to the CUDA root dir
 # cmake -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc -DCMAKE_CUDA_COMPILER_TOOLKIT_ROOT=/usr/local/cuda/ ..
 cmake ..
-make
+make -j$(nproc)
 cd -
 ln -s build/peakperf peakperf
