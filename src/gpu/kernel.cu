f03b3b4cbb1a42cb3c8259baafc197b8cbad5588
Dr-Noob <peibolms@gmail.com>
2024-05-15 21:54:39 +0200

    Better performance in ada

--- src/gpu/kernel.cu
+++ src/gpu/kernel.cu
@@ -4,37 +4,38 @@
 
 __global__
 void compute_kernel(float *vec_a, float *vec_b, float *vec_c, int n) {
-  int cid = threadIdx.x + blockIdx.x * blockDim.x;
-  int tid = threadIdx.x;
+  int gid = threadIdx.x + blockIdx.x * blockDim.x;
+  int off = gid*4;
   __shared__ float myblockA[4];
   __shared__ float myblockB[4];
-  float c1 = vec_c[0];
-  float c2 = vec_c[1];
-  float c3 = vec_c[2];
-  float c4 = vec_c[3];
+  __shared__ float myblockC[4];
 
-  myblockA[0] = vec_a[0];
-  myblockB[0] = vec_b[0];
-  myblockA[1] = vec_a[1];
-  myblockB[1] = vec_b[1];
-  myblockA[2] = vec_a[2];
-  myblockB[2] = vec_b[2];
-  myblockA[3] = vec_a[3];
-  myblockB[3] = vec_b[3];
+  myblockA[0] = vec_a[off+0];
+  myblockB[0] = vec_b[off+0];
+  myblockC[0] = vec_a[off+0];
+  myblockA[1] = vec_a[off+1];
+  myblockB[1] = vec_b[off+1];
+  myblockC[1] = vec_a[off+1];
+  myblockA[2] = vec_a[off+2];
+  myblockB[2] = vec_b[off+2];
+  myblockC[2] = vec_a[off+2];
+  myblockA[3] = vec_a[off+3];
+  myblockB[3] = vec_b[off+3];
+  myblockC[3] = vec_a[off+3];
 
   __syncthreads();
 
-  #pragma unroll 2000
+  #pragma unroll 32
   for(long i=0; i < BENCHMARK_GPU_ITERS; i++) {
-    c1 = (c1 * myblockA[0]) + myblockB[0];
-    c2 = (c2 * myblockA[1]) + myblockB[1];
-    c3 = (c3 * myblockA[2]) + myblockB[2];
-    c4 = (c4 * myblockA[3]) + myblockB[3];
+    myblockC[0] = (myblockC[0] * myblockA[0]) + myblockB[0];
+    myblockC[1] = (myblockC[1] * myblockA[1]) + myblockB[1];
+    myblockC[2] = (myblockC[2] * myblockA[2]) + myblockB[2];
+    myblockC[3] = (myblockC[3] * myblockA[3]) + myblockB[3];
   }
 
-  vec_c[0] = c1;
-  vec_c[1] = c2;
-  vec_c[2] = c3;
-  vec_c[3] = c4;
+  vec_c[off+0] = myblockC[0];
+  vec_c[off+1] = myblockC[1];
+  vec_c[off+2] = myblockC[2];
+  vec_c[off+3] = myblockC[3];
 }
 
